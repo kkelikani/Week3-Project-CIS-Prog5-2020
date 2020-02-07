1a811e340dff6c4bbc9da96cec5ef72c6a1a0631
kkelikani <60380114+kkelikani@users.noreply.github.com>
2020-02-07 10:05:13 -0800

    Add files via upload

---
+++ momentum.py
@@ -0,0 +1,21 @@
+"""
+Program: That accepts mass in kg to come up with objects momentum
+Author: Kaleo Kelikani
+
+1.Compute mass * velocity
+
+2. Input = mass kg
+
+3.Computations are:
+
+# Formulas to obtain objects momentum is mass x velocity
+
+"""
+# Request input for the radius of the sphere
+mass=int(input("Enter the size of the mass is kg of object:  "))
+mass
+velocity=int(input("Enter the velocity in meters per second:  "))
+momentum=(mass * velocity)
+print("The momentum of the object is :  ", momentum,"kg m/s")
+
+
