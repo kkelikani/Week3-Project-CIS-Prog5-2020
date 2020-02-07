f3419aace5cd778a9787d73a3fd15ba11177297b
kkelikani <60380114+kkelikani@users.noreply.github.com>
2020-02-07 10:36:34 -0800

    Add files via upload

---
+++ minutesinayear.py
@@ -0,0 +1,31 @@
+"""
+Program: That calculate and prints the number of minutes in a year
+
+Author: Kaleo Kelikani
+
+1.Compute number of (minutes per hour)(hours per day)(days in a year)
+
+3.Computations are:
+
+#
+"""
+# Constrictions are
+#60 minutes in a hour, 24 hours in a day, 365 days in a year
+# enter constrictions in interger format 
+minutes=int(60)
+hours=int(24)
+days=int(365)# Constrictions are
+#60 minutes in a hour, 24 hours in a day, 365 days in a year
+# enter constrictions in interger format 
+minutes=int(60)
+minutes
+hours=int(24)
+hours
+days=int(365)
+days
+year=(minutes * hours * days )
+year
+print ("Number of minutes in a year " , year )
+
+
+
