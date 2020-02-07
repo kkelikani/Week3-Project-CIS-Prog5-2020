85b91e0cb7e270a050cb95ba2d8b500e7f9c121f
kkelikani <60380114+kkelikani@users.noreply.github.com>
2020-02-07 09:20:36 -0800

    Add files via upload

---
+++ areaofcube.py
@@ -0,0 +1,19 @@
+"""
+Program: Area of a Cube
+Author: Kaleo Kelikani
+Compute the area of a Cube
+Formula of the Area of a cuber is A=s * s * s
+Input one side of the cube
+Compute s=10   area= 10 * 10 * 10
+area(int(input("Enter the size of one side of the cube"))
+print("Area of the cube is:" , area
+
+
+"""
+# Request input one side of the cube
+side = int(input("Enter the size of one side of the cube:  "))
+side
+# Apply A=s3 formula
+area = ((side * side) * (side))
+area
+print("Area of the cube is :  " , area)
